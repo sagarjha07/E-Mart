767769fc303bb7d332e3908491d7bd8ccc51ed86
Sagar kumar jha <jha8768@gmail.com>
2022-01-12 22:31:34 +0530

    Error Handling

--- Back-end/controllers/productController.js
+++ Back-end/controllers/productController.js
@@ -1,37 +1,32 @@
 const Product = require("../models/productModel");
+const catchAsyncError = require("../middleware/catchAsync");
+const ErrorHandler = require("../utils/errorHandler");
 
-module.exports.getProduct = async (req, res) => {
+module.exports.getProduct = catchAsyncError(async (req, res) => {
 	const product = await Product.findById(req.params.id);
 	return res.status(200).json({
 		success: true,
 		product,
 	});
-};
+});
 
-module.exports.getAllProducts = async (req, res) => {
+module.exports.getAllProducts = catchAsyncError(async (req, res) => {
 	const product = await Product.find({});
 	return res.status(200).json({
 		success: true,
 		product,
 	});
-};
+});
 
-module.exports.createProduct = async (req, res) => {
-	try {
-		const newproduct = await Product.create(req.body);
-		return res.status(201).json({
-			success: true,
-			product: newproduct,
-		});
-	} catch (error) {
-		return res.status(404).json({
-			success: false,
-			err: error.message,
-		});
-	}
-};
+module.exports.createProduct = catchAsyncError(async (req, res) => {
+	const product = await Product.create(req.body);
+	return res.status(201).json({
+		success: true,
+		product,
+	});
+});
 
-module.exports.updateProduct = async (req, res) => {
+module.exports.updateProduct = catchAsyncError(async (req, res) => {
 	let product = await Product.findById(req.params.id);
 	product = await Product.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
@@ -42,11 +37,11 @@ module.exports.updateProduct = async (req, res) => {
 		success: true,
 		product,
 	});
-};
+});
 
-module.exports.removeProduct = async (req, res) => {
+module.exports.removeProduct = catchAsyncError(async (req, res) => {
 	await Product.findByIdAndDelete(req.params.id);
 	return res.status(200).json({
 		success: true,
 	});
-};
+});
--- Back-end/index.js
+++ Back-end/index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const routes = require("./routes/index");
+const errorMiddleware = require("./middleware/error");
+
 dotenv.config({ path: "./config/.env" });
 
 const app = express();
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+	console.log(`Error: ${err.message}`);
+	console.log(`Shutting down the server due to Uncaught Exception`);
+	process.exit(1);
+});
+
 
 mongoose
 	.connect(process.env.DB_URL, {
@@ -12,16 +22,26 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then(() => {
-		app.listen(process.env.PORT, () => {
-			console.log(
-				"DB connected and Server is Running at PORT: ",
-				process.env.PORT
-			);
-		});
+		console.log("DB connected sucessfully!!");
 	})
 	.catch((err) => {
 		console.log("Error in DB connection ", err);
 	});
 
 app.use(express.json());
+app.use(errorMiddleware);
 app.use(routes);
+
+const server = app.listen(process.env.PORT, () => {
+	console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+	console.log(`Error: ${err.message}`);
+	console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+	server.close(() => {
+		process.exit(1);
+	});
+});
